be854aafc2c06a1b11299da220429c72aee9606b
Pablo Gutierrez <pablo_gutierrez@32991.gnm.int>
2019-01-25 16:01:13 +0000

    populist x axis

---
+++ src/js/unique.js
@@ -0,0 +1,150 @@
+import * as d3B from 'd3'
+import * as d3Select from 'd3-selection'
+import {event as currentEvent} from 'd3-selection';
+import * as d3Queue from 'd3-queue'
+import { $ } from "./util"
+
+let d3 = Object.assign({}, d3B, d3Select, d3Queue);
+const mapEl = $(".interactive-wrapper");
+const dataURL = '<%= path %>/assets/Copy of EDITED Chief Executives MASTER final scores - UNIQUE_40.csv';
+const firstYear = 1998;
+const lastYear = 2018;
+const countries = [];
+let nodes = [];
+let currentYear = firstYear;
+
+d3.select(".unique-title").html(currentYear)
+
+let width = mapEl.getBoundingClientRect().width;
+let height = 300;
+
+let svg = d3.select(".unique-svg")
+.append('svg')
+.attr("width", width)
+.attr("height", height)
+
+let lineGenerator = d3.line();
+let points = [
+[0,height/2],
+[width,height/2]
+]
+
+let bees;
+let pathData = lineGenerator(points);
+
+let axis = svg.append('path')
+.attr('d', pathData)
+.attr('class', 'axis');
+
+let center = {x: 0, y:0}
+
+let forceStrength = 0.02;
+
+let simulation = d3.forceSimulation()
+.velocityDecay(0.2)
+.force('x', d3.forceX().strength(forceStrength).x(center.x))
+.force('y', d3.forceY().strength(forceStrength).y(center.y))
+.force('charge', d3.forceManyBody().strength(charge))
+.force("collide", d3.forceCollide().radius(d => d.radius))
+.on('tick', beesTicked);
+
+Promise.all([
+	d3.csv(dataURL)
+	])
+.then(ready)
+
+function ready(csv){
+
+	let data = csv[0];
+	let countryName = null;
+
+	d3.map(data, d => {
+		if(countryName != d.country){
+			countryName = d.country
+			countries.push({country: countryName, region: d.region.split('&').join('and')})
+		}
+	});
+
+	countries.forEach(c => {
+
+		let speechCategory = null;
+
+		let node =  {
+			country:c.country.split(' ').join('-'),
+			region:c.region,
+	        populist1998:speechCategory,
+	        populist1999:speechCategory,
+	        populist2000:speechCategory,
+	        populist2001:speechCategory,
+	        populist2002:speechCategory,
+	        populist2003:speechCategory,
+	        populist2004:speechCategory,
+	        populist2005:speechCategory,
+	        populist2006:speechCategory,
+	        populist2007:speechCategory,
+	        populist2008:speechCategory,
+	        populist2009:speechCategory,
+	        populist2010:speechCategory,
+	        populist2011:speechCategory,
+	        populist2012:speechCategory,
+	        populist2013:speechCategory,
+	        populist2014:speechCategory,
+	        populist2015:speechCategory,
+	        populist2016:speechCategory,
+	        populist2017:speechCategory,
+	        populist2018:speechCategory,
+	        radius:5.5
+      };
+      	let country = data.filter(d => d.country == c.country)
+		let years = country.map(c => +c.yearbegin)
+		let populism = country.map(c => c.totalaverage)
+		let localPopulism = speechCategory;
+
+		for (var i = firstYear; i <= lastYear; i++) {
+
+			if(years.indexOf(i) != -1){
+				localPopulism = populism[years.indexOf(i)];
+			}
+
+			node['populist' + i] = localPopulism
+		}
+
+		nodes.push(node)
+	})
+
+	simulation.nodes(nodes)
+
+	bees = svg.selectAll('circle')
+	.data(nodes)
+	.enter()
+	.append('circle')
+	.attr('class', d => d.region)
+	.attr('r', d => d.radius)
+
+	setInterval(d => {
+		currentYear++
+		if(currentYear > lastYear) currentYear = firstYear;
+		simulation.force('x', d3.forceX().strength(forceStrength).x(nodePopulismPosX))
+		d3.select(".unique-title").html(currentYear)
+		simulation.alpha(1).restart();
+	}, 2000);
+
+
+}
+
+function nodePopulismPosX(d) {
+  return (width * d['populist'+currentYear] / 2)
+}
+
+
+function beesTicked() {
+  bees
+  .attr('cx', d => d.x + 50)
+  .attr('cy', d => d.y + height /2)
+}
+
+
+
+function charge(d) {
+    return Math.pow(d.radius, 2.0) * forceStrength;
+}
